01b912ee630b16aa76734c7d309e75b7cf203a43
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-05-24 19:51:20 +0700

    fullminigames

--- Assets/Script/Script/MiniGames/MiniGameManager.cs
+++ Assets/Script/Script/MiniGames/MiniGameManager.cs
@@ -5,24 +5,18 @@ public class MiniGameManager : MonoBehaviour
     public GameObject mainCamera;
     public GameObject miniGameCamera;
     public GameObject pcbOnTable;
-    public GameObject panelMiniGameUI;
 
-    // Tambah referensi UI gameplay utama yang mau disembunyikan sementara
-    public GameObject textTimer;
-    public GameObject textTabungan;
-    public GameObject textUpah;
+    public GameObject miniGameUIPanel;   // Panel UI mini game
+    public GameObject gameplayUI;         // UI gameplay utama (timer, tabungan, dll)
 
     public void StartMiniGame()
     {
         mainCamera.SetActive(false);
         miniGameCamera.SetActive(true);
         pcbOnTable.SetActive(true);
-        panelMiniGameUI.SetActive(true);
 
-        // Hide UI gameplay utama sementara
-        textTimer.SetActive(false);
-        textTabungan.SetActive(false);
-        textUpah.SetActive(false);
+        miniGameUIPanel.SetActive(true);
+        gameplayUI.SetActive(false);
     }
 
     public void EndMiniGame()
@@ -30,11 +24,9 @@ public class MiniGameManager : MonoBehaviour
         miniGameCamera.SetActive(false);
         mainCamera.SetActive(true);
         pcbOnTable.SetActive(false);
-        panelMiniGameUI.SetActive(false);
 
-        // Show UI gameplay utama kembali
-        textTimer.SetActive(true);
-        textTabungan.SetActive(true);
-        textUpah.SetActive(true);
+        miniGameUIPanel.SetActive(false);
+        gameplayUI.SetActive(true);
     }
 }
+
