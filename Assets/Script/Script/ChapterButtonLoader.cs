cc1f15486bcb25a8468ad08d35a5f99e06b6b8c3
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-06-26 12:01:20 +0700

    1

---
+++ Assets/Script/Script/ChapterButtonLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChapterButtonLoader : MonoBehaviour
+{
+    public string sceneToLoad; // isi di Inspector
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Debug.Log("📥 Load scene: " + sceneToLoad);
+            SceneManager.LoadScene(sceneToLoad);
+        });
+    }
+}
