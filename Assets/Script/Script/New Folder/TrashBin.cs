b1a04169b84c619e70cfd58b84b788d573a4d26b
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-07-06 15:44:48 +0700

    1

--- Assets/Script/Script/New Folder/TrashBin.cs
+++ Assets/Script/Script/New Folder/TrashBin.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 
+
+
 public class TrashBin : MonoBehaviour
 {
     public string acceptedTag = "Ewaste"; // Ganti sesuai tag objek yang ingin dibuang
+
+    public BoxPenyimpanan boxPenyimpanan;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(acceptedTag))
         {
             Debug.Log("Item berhasil dibuang ke tong sampah!");
+
+            // Panggil AddItem di BoxPenyimpanan
+            if (boxPenyimpanan != null)
+            {
+                boxPenyimpanan.AddItem(other.tag); // atau other.name jika kamu ingin lebih spesifik
+            }
+
             Destroy(other.gameObject); // Hapus item setelah dibuang
         }
         else
@@ -16,4 +27,5 @@ public class TrashBin : MonoBehaviour
             Debug.Log("Item ini tidak bisa dibuang di tong ini.");
         }
     }
+
 }
