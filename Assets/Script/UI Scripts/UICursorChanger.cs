4fcb4b92ed7c2283eedd59d3d9c2662d0a1f5c7f
Fikri Ahmad <rukimevick@gmail.com>
2025-07-07 11:15:28 +0700

    Fixing Cursor Manager

--- Assets/Script/UI Scripts/UICursorChanger.cs
+++ Assets/Script/UI Scripts/UICursorChanger.cs
@@ -7,15 +7,18 @@ public class UICursorChanger : MonoBehaviour, IPointerEnterHandler,IPointerExitH
     public void OnPointerEnter(PointerEventData eventData)
     {
         CursorManager.Instance.SetGrabableCursor();
+        Debug.Log("Pointer entered");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         CursorManager.Instance.SetDefaultCursor();
+        Debug.Log("Pointer exited");
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         CursorManager.Instance.StopDragging();
+        Debug.Log("Pointer up");
     }
 }
