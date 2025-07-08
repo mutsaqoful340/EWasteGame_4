dc6b5902ab1c8886deca8eb21c7d61cb66b3a823
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-07-08 18:57:14 +0700

    1

--- Assets/Script/MiniGames/Kenang/GamemanageKenang.cs
+++ Assets/Script/MiniGames/Kenang/GamemanageKenang.cs
@@ -1,28 +1,54 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerKenang : MonoBehaviour
 {
     public static GameManagerKenang instance;
 
+    [Header("Panel UI")]
+    public GameObject panelStart;
+    public GameObject panelAkhir;
+
+    [Header("Scene Berikutnya")]
+    public string namaSceneBerikutnya;
+
+    [Header("Jumlah Komponen")]
     public int totalItem = 10;
     private int currentDrop = 0;
 
-    public GameObject panelAkhir;
     public bool sudahSelesai = false;
+    private bool gameDimulai = false;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
+        {
             Destroy(gameObject);
+            return;
+        }
+
+        if (panelStart != null)
+            panelStart.SetActive(true);
 
         if (panelAkhir != null)
             panelAkhir.SetActive(false);
     }
 
+    // Dipanggil oleh tombol "Start"
+    public void MulaiPermainan()
+    {
+        gameDimulai = true;
+
+        if (panelStart != null)
+            panelStart.SetActive(false);
+    }
+
     public void TambahItemMasuk()
     {
+        if (!gameDimulai) return;
+
         currentDrop++;
         Debug.Log("Item masuk: " + currentDrop + "/" + totalItem);
 
@@ -37,5 +63,17 @@ public class GameManagerKenang : MonoBehaviour
     {
         if (panelAkhir != null)
             panelAkhir.SetActive(true);
+    }
+
+    public void NextScene()
+    {
+        if (!string.IsNullOrEmpty(namaSceneBerikutnya))
+        {
+            SceneManager.LoadScene(namaSceneBerikutnya);
+        }
+        else
+        {
+            Debug.LogWarning("Nama scene belum diisi di Inspector!");
+        }
     }
 }
