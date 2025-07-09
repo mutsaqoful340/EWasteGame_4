7047f7a4ca1e4d27f7458bd00d65b37dbc4b2095
Fikri Ahmad <rukimevick@gmail.com>
2025-07-09 17:41:39 +0700

    Exit Game Script (.cs)

---
+++ Assets/Script/App_Exit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App_Exit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
