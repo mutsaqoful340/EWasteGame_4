068d5ff4a2d4f7b7210ce2cdc511e681f201393a
Fikri Ahmad <rukimevick@gmail.com>
2025-07-14 12:11:24 +0700

    Update PlayerInteraction.cs

--- Assets/Script/FPS Movement/PlayerInteraction.cs
+++ Assets/Script/FPS Movement/PlayerInteraction.cs
@@ -1,3 +1,4 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 using TMPro; // If you use TextMeshPro
 
@@ -7,13 +8,22 @@ public class PlayerInteraction : MonoBehaviour
     public FirstPersonController fpsController; // Your movement script
     public Cinemachine.CinemachineVirtualCamera freeRoamVCam;
     
-
     [Header("UI")]
     public TextMeshProUGUI hoverUIText;
     public Animator GPUI_Aniamtor;
+    public Animator HPMenuAnimator; // Animator for HP menu
 
+    private CharacterController controller;
+    private PlayerControls inputActions;
     private GameplayPoint nearbyGP;
     private bool inGPMode = false;
+    private bool isHPOpened = false; // For HP menu toggle
+
+    private void Awake()
+    {
+        inputActions = new PlayerControls();
+        inputActions.Player.Enable();
+    }
 
     void Update()
     {
@@ -73,6 +83,55 @@ public class PlayerInteraction : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    public void CloseHPMenu()
+    {
+        if (isHPOpened)
+        {
+            isHPOpened = false;
+            HPMenuAnimator.Play("HPMenu_OUT");
+            fpsController.enabled = true; // Re-enable player controls when HP menu is closed
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.MenuHP.performed += ctx => OnHPMenuToggle();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.MenuHP.performed -= ctx => OnHPMenuToggle();
+    }
+
+    private void OnHPMenuToggle()
+    {
+        // If the menu is about to open AND you’re in GP mode → don’t do anything
+        if (!isHPOpened && inGPMode) return; // Don't toggle HP menu if in GP mode
+        isHPOpened = !isHPOpened;
+
+        if (isHPOpened)
+        {
+            HPMenuAnimator.Play("HPMenu_IN");
+            //Time.timeScale = 0.01f;
+            fpsController.enabled = false; // Disable player controls when HP menu is open
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            HPMenuAnimator.Play("HPMenu_OUT");
+            //Time.timeScale = 1f;
+            fpsController.enabled = true; // Re-enable player controls when HP menu is closed
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     void LateUpdate()
