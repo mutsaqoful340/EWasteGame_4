441f3d26db65f524f68daf1b421ca5bbe0d8e819
Fikri Ahmad <rukimevick@gmail.com>
2025-07-16 17:37:05 +0700

    Modifying VCamChanger

--- Assets/Script/3DMenu/VCam_Changer.cs
+++ Assets/Script/3DMenu/VCam_Changer.cs
@@ -6,6 +6,7 @@ public class VCam_Changer : MonoBehaviour
 {
     [Header("Virtual Camera")]
     public Cinemachine.CinemachineVirtualCamera currentVCam;
+    public float VCamDelay = 0.5f; // Delay before switching VCam
 
 
     public void VCam_IN()
@@ -16,6 +17,18 @@ public class VCam_Changer : MonoBehaviour
 
     public void VCam_OUT()
     {
+        //Cinemachine.CinemachineVirtualCamera.activeCamera.Priority = 5;
+        currentVCam.Priority = 5;
+    }
+    public void GPVCam_OUT(GameplayPoint gp)
+    {
+        gp.DeactivateGameplay();
+    }
+
+
+    private IEnumerator Co_VCam_OUT(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         currentVCam.Priority = 5;
     }
 }
