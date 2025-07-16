625a95e0e22dee1e675adc0bac888def8e85ba18
Fikri Ahmad <rukimevick@gmail.com>
2025-07-16 17:37:22 +0700

    Create VolumeSlider for Slider usage

---
+++ Assets/Script/Audio Scripts/VolumeSlider.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer myMixer;  // Same mixer as knob
+    public VolumeType volumeType = VolumeType.BGM;  // Same enum!
+
+    public Slider slider;
+
+    private string ExposedParamName => volumeType.ToString();
+
+    private float minVolume = 0.0001f;
+    private float maxVolume = 1f;
+
+    private void Start()
+    {
+        if (slider == null)
+            slider = GetComponent<Slider>();
+
+        float savedVolume = PlayerPrefs.GetFloat(ExposedParamName + "Volume", 0.5f);
+        slider.value = savedVolume;
+
+        SetVolume(savedVolume);
+
+        slider.onValueChanged.AddListener(SetVolume);
+    }
+
+    private void SetVolume(float volume01)
+    {
+        volume01 = Mathf.Clamp(volume01, minVolume, maxVolume);
+        myMixer.SetFloat(ExposedParamName, Mathf.Log10(volume01) * 20f);
+        PlayerPrefs.SetFloat(ExposedParamName + "Volume", volume01);
+    }
+}
