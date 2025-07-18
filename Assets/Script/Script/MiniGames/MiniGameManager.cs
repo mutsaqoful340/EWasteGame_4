fc6639c17ae78f08809059837bac6f03ab3ecd34
Fikri Ahmad <rukimevick@gmail.com>
2025-07-18 16:48:56 +0700

    Update MiniGameManager.cs

--- Assets/Script/Script/MiniGames/MiniGameManager.cs
+++ Assets/Script/Script/MiniGames/MiniGameManager.cs
@@ -3,46 +3,17 @@ using System.Collections;
 
 public class MiniGameManager : MonoBehaviour
 {
-
-    public CameraZoomTransition zoomTransition;
-    public GameObject mainCamera;
-    public GameObject miniGameCamera;
-    public GameObject pcbOnTable;
-
-    public GameObject miniGameUIPanel;   // Panel UI mini game
-    public GameObject gameplayUI;         // UI gameplay utama (timer, tabungan, dll)
-
+    public GameObject hpMiniGame;
+    public GameObject hpMeja;
     public void StartMiniGame()
     {
-        Debug.Log("StartMiniGame() dipanggil");
-
-        mainCamera.SetActive(false);
-        miniGameCamera.SetActive(true);
-
-        pcbOnTable.SetActive(true);
-        miniGameUIPanel.SetActive(true);
-
-        gameplayUI.SetActive(false); // tambahkan log setelah ini juga
-        Debug.Log("gameplayUI disembunyikan");
-
-        StartCoroutine(StartZoomDelayed());
-    }
-
-    IEnumerator StartZoomDelayed()
-    {
-        yield return new WaitForSeconds(0.1f); // beri waktu 1 frame setelah kamera aktif
-        if (zoomTransition != null)
-            zoomTransition.StartZoom();
+        hpMiniGame.SetActive(true);
+        hpMeja.SetActive(true);
     }
 
     public void EndMiniGame()
     {
-        miniGameCamera.SetActive(false);
-        mainCamera.SetActive(true);
-        pcbOnTable.SetActive(false);
-
-        miniGameUIPanel.SetActive(false);
-        gameplayUI.SetActive(true);
+        hpMiniGame.SetActive(false);
+        hpMeja.SetActive(false);
     }
 }
-
